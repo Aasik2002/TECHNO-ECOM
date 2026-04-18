f686a763ce230faabaca30948ccedb79f6a508c1
Aasik2002 <aasikfa2002@gmail.com>
2026-04-19 02:40:19 +0530

    feat: admin get all orders and deliver status logic completed

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -96,3 +96,35 @@ export const getMyOrders = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+// @desc    Get all orders (Admin only)
+// @route   GET /api/orders
+// @access  Private/Admin
+export const getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({}).populate("user", "id name");
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private/Admin
+export const updateOrderToDelivered = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+      order.isDelivered = true;
+      order.deliveredAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404).json({ message: "Order not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -4,13 +4,17 @@ import {
   addOrderItems, 
   getOrderById, 
   updateOrderToPaid, 
-  getMyOrders 
+  getMyOrders,
+  getOrders,
+  updateOrderToDelivered
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
 router.route("/myorders").get(protect, getMyOrders); 
+router.route("/").get(protect, getOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, updateOrderToDelivered);
 
 export default router;
